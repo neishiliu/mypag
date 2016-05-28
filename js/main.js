a7b32fb414e450dd5e964bbf494bf7a3ba0e2dd1
neishiliu <liuqiang0832@foxmail.com>
2016-05-28 14:55:37 +0800

    完成全部静态页面

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by Administrator on 2016/5/26.
  */
+$(function(){
+    setInterval("changeHeight()",100) ;
+});
+
+//更改主页高度，使其沾满浏览器窗口
+function changeHeight(){
+    var win_height = document.documentElement.clientHeight ;
+    var new_height = win_height - "50";
+    $("#head").css({"height":new_height});
+}
+
