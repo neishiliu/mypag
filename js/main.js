265394a8f5eca3911bffa343a1e3f64075f9125d
neishiliu <liuqiang0832@foxmail.com>
2016-05-29 00:00:05 +0800

    添加动态样式

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
  */
 $(function(){
     setInterval("changeHeight()",100) ;
+    setInterval("getSize()",100)
 });
 
 //更改主页高度，使其沾满浏览器窗口
@@ -11,4 +12,12 @@ function changeHeight(){
     var new_height = win_height - "50";
     $("#head").css({"height":new_height});
 }
-
+//获取图片尺寸，并附给翻转背景
+function getSize(){
+    var $img_height = $(".work").height();
+    var $img_width = $(".work").width();
+    $(".works-hover").height($img_height).width($img_width);
+    var $contact_img_height = $(".git").height();
+    var $contact_img_width = $(".git").width();
+    $(".contact-hover-main").height($contact_img_height).width($contact_img_width);
+}
